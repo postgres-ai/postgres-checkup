2e32cd93507d4c97a7756f093c2a27ea92e6f7c4
dmius <uddm@mail.ru>
2018-11-19 14:38:54 +0300

    Work on a003 report started

--- pghrep/plugins/a003.go
+++ pghrep/plugins/a003.go
@@ -1,15 +1,47 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "../src/pyraconv"
+    "../src/fmtutils"
+    "strconv"
+)
 
 var Data map[string]interface{}
 
 type prepare string
 
+func getSharefBuffersValue(hostSettings []interface{}) int64 {
+    for _, setting := range hostSettings {
+        curSetting := setting.(map[string]interface{})
+        if curSetting["name"] == "shared_buffers" {
+            if curSetting["unit"] != nil {
+                val, err := strconv.ParseInt(pyraconv.ToString(curSetting["setting"]), 10, 64)
+                if err != nil {
+                    return -1
+                }
+                unit := fmtutils.GetUnit(pyraconv.ToString(curSetting["unit"]));
+                if unit != -1 {
+                    val = val * unit
+                }
+                return val
+            }
+        }
+    }
+    return -1
+}
+
 func (g prepare) Prepare(data map[string]interface{}) map[string]interface{} {
-    fmt.Println("Hello from A011!", data)
     result := make(map[string]interface{})
-    result["current"] = "CURRENT VALUES"
+    hosts := pyraconv.ToStringArray(data["hosts"])
+    checkData := data["checkData"].(map[string]interface{})
+    for _, host := range hosts {
+        fmt.Println("Host:", host)
+        hostSettings := checkData[host].([]interface{})
+        shared_buffers := getSharefBuffersValue(hostSettings)
+        result["current"] = "Current value of settings shared_buffers is " + fmtutils.ByteFormat(float64(shared_buffers), 0)
+    }
+
     result["recommended"] = "RECOMMENDED VALUES"
     result["conclusion"] = "CONCLUSION VALUES"
     result["filename"] = "a011_shared_buffers.md"
