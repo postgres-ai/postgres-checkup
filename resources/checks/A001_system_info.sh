800b4dbfeca6df5e6bb7e1ef58833986a66a7e09
dmius <uddm@mail.ru>
2018-11-21 14:13:22 +0300

    ssh support + bug fix

--- resources/checks/A001_system_info.sh
+++ resources/checks/A001_system_info.sh
@@ -9,8 +9,8 @@ CTL_INFO=""
 function get_cpu_info() {
   local host=$1
   local res=""
-  #local cpu_info="$(ssh "$host" "lscpu")"
-  local cpu_info="$("lscpu")"
+  local cpu_info="$(ssh "$host" "lscpu")"
+  #local cpu_info="$("lscpu")"
   cpu_info="${cpu_info/\"/\\\"}"
   local res_obj="{\"cmd2check\": \"lscpu\""
   while read -r line; do
@@ -26,8 +26,8 @@ function get_cpu_info() {
 function get_mem_info() {
   local host=$1
   local res=""
-  #local mem_info="$(ssh "$host" "cat /proc/meminfo" | sed 's/"/\\"/g')"
-  local mem_info="$(cat /proc/meminfo)"
+  local mem_info="$(ssh "$host" "cat /proc/meminfo")"
+  #local mem_info="$(cat /proc/meminfo)"
   mem_info="${mem_info/\"/\\\"}"
   local res_obj="{\"cmd2check\": \"cat /proc/meminfo\""
   while read -r line; do
@@ -42,16 +42,16 @@ function get_mem_info() {
 
 function get_system_info() {
   local host=$1
-  #local sys_info="$(ssh "$host" "uname -a")"
-  local sys_info="$(uname -a | sed 's/"/\\"/g')"
+  local sys_info="$(ssh "$host" "uname -a")"
+  #local sys_info="$(uname -a | sed 's/"/\\"/g')"
   res_obj="{\"cmd2check\": \"uname -a\", \"raw\": \"$sys_info\"}"
   OS_INFO=$res_obj #$(jq -n "$res_obj")
 }
 
 function get_ctl_info() {
   local host=$1
-  #local sys_info="$(ssh "$host" "hostnamectl status")"
-  local ctl_info="$(hostnamectl status | sed 's/"/\\"/g')"
+  local sys_info="$(ssh "$host" "hostnamectl status")"
+  #local ctl_info="$(hostnamectl status)"
   ctl_info="${ctl_info/\"/\\\"}"
   local res_obj="{\"cmd2check\": \"hostnamectl status\""
   while read -r line; do
@@ -66,8 +66,8 @@ function get_ctl_info() {
 
 function get_disk_info() {
   local host=$1
-  #local sys_info="$(ssh "$host" "dt -T" | sed 's/"/\\"/g')"
-  local disk_info="$(df -T | sed 's/"/\\"/g')"
+  local sys_info="$(ssh "$host" "df -T")"
+  #local disk_info="$(df -T | sed 's/"/\\"/g')"
   res_obj="{\"cmd2check\": \"df -T\", \"raw\": \"$disk_info\"}"
   DISK_INFO=$res_obj #$(jq -n "$res_obj")
 }
@@ -80,7 +80,7 @@ get_disk_info $HOST
 get_ctl_info $HOST
 host_obj="{\"cpu\": $CPU_INFO, \"ram\": $MEM_INFO, \"system\": $OS_INFO, \"disk\": $DISK_INFO}"
 host_obj="{\"cpu\": $CPU_INFO, \"ram\": $MEM_INFO, \"system\": $OS_INFO, \"disk\": $DISK_INFO, \"virtualization\": $CTL_INFO }"
-result="{\"$host\": ${host_obj}}"
+result="{\"$HOST\": ${host_obj}}"
 result="[${result}]"
 
 result=$(jq -n "$result")
