7c71dbd95513607d2ababb1a0415e2b84e75ef37
dmius <uddm@mail.ru>
2018-11-22 17:18:29 +0300

    Fix json structure of pg_settings data

--- resources/checks/A003_pgsettings.sh
+++ resources/checks/A003_pgsettings.sh
@@ -1,4 +1,5 @@
 # Collect pg_settings artifact
 dbg "PSQL_CONN_OPTIONS: ${PSQL_CONN_OPTIONS}"
-psql ${PSQL_CONN_OPTIONS} -c "select row_to_json(row) from (select * from pg_settings) row"
+#psql ${PSQL_CONN_OPTIONS} -c "select row_to_json(row) from (select * from pg_settings) row"
+psql ${PSQL_CONN_OPTIONS} -c "select json_build_object(row.name, row) from (select * from pg_settings) row"
 
