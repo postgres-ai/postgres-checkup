9d0e4a3bbac4c44700077376f2a42c98381cbe7f
dmius <uddm@mail.ru>
2018-11-23 11:36:01 +0300

    Check A003 fixed, A004 added, A005 added, check tool fixed (#78)
    * Check A003 fixed, A004 added, A005 added, check tool fixed
    
    * jq - removed array from `results`

--- resources/checks/A003_pgsettings.sh
+++ resources/checks/A003_pgsettings.sh
@@ -1,4 +1,4 @@
 # Collect pg_settings artifact
 dbg "PSQL_CONN_OPTIONS: ${PSQL_CONN_OPTIONS}"
-psql ${PSQL_CONN_OPTIONS} -c "select row_to_json(row) from (select * from pg_settings) row"
+psql ${PSQL_CONN_OPTIONS} -c "select json_object_agg(s.name, s) from pg_settings s;"
 
