90b7a17792104233e4610e11947187b1fdb9b5d8
dmius <uddm@mail.ru>
2018-11-26 14:26:55 +0300

    Params fixed

--- resources/checks/A006_pg_conf.sh
+++ resources/checks/A006_pg_conf.sh
@@ -1,6 +1,6 @@
 # Collect pg_settings and pg_config values data to compare with same values from other domains
 # Require comparation in plugin
-pg_settings=$(ssh ${HOST} "${_PSQL} -c \"select json_object_agg(s.name, s) from pg_settings s;\" -t -A")
-pg_config=$(ssh ${HOST} "${_PSQL} -c \"select json_object_agg(c.name, c) from pg_config c;\" -t -A")
+pg_settings=$(ssh ${HOST} "${_PSQL} -c \"select json_object_agg(s.name, s) from pg_settings s;\"")
+pg_config=$(ssh ${HOST} "${_PSQL} -c \"select json_object_agg(c.name, c) from pg_config c;\"")
 echo '{ "pg_settings": '$pg_settings', "pg_config":'$pg_config'}'
 
