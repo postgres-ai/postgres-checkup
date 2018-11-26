00809f200439907e0943efd14958fd01ddbf981c
dmius <uddm@mail.ru>
2018-11-26 14:03:13 +0300

    PSQL connection fix

--- resources/checks/A006_pg_conf.sh
+++ resources/checks/A006_pg_conf.sh
@@ -1,7 +1,6 @@
 # Collect pg_settings artifact
 # Require comparation in plugin
-dbg "PSQL_CONN_OPTIONS: ${PSQL_CONN_OPTIONS}"
-pg_settings=$(psql ${PSQL_CONN_OPTIONS} -c "select json_object_agg(s.name, s) from pg_settings s;" -t -A)
-pg_config=$(psql ${PSQL_CONN_OPTIONS} -c "select json_object_agg(c.name, c) from pg_config c;" -t -A)
+pg_settings=$(ssh ${HOST} "${_PSQL} -c \"select json_object_agg(s.name, s) from pg_settings s;\" -t -A")
+pg_config=$(ssh ${HOST} "${_PSQL} -c \"select json_object_agg(c.name, c) from pg_config c;\" -t -A")
 echo '{ "pg_settings": '$pg_settings', "pg_config":'$pg_config'}'
 
