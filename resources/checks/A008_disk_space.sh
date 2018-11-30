0483d5ba8edbcea6cc9dcb1e37791d78c7061983
Victor Yagofarov <xnasx@yandex.ru>
2018-11-30 18:29:22 +0300

    #19 first example of A008 check

--- resources/checks/A008_disk_space.sh
+++ resources/checks/A008_disk_space.sh
@@ -1,5 +1,4 @@
-
-set -x
+# Check disk space for important for postgres disk partitions
 
 PG_MAJOR_VER=$(ssh "$HOST" "${_PSQL} -f -" <<EOF
   select setting::integer / 10000 from pg_settings where name = 'server_version_num'
@@ -12,17 +11,17 @@ EOF
 )
 PG_DATA_DIR="data_directory: ${PG_DATA_DIR}"
 
-PG_STATS_TEMP_DIRECTORY=$(ssh "$HOST" "${_PSQL} -f -" <<EOF
+PG_STATS_TEMP_DIR=$(ssh "$HOST" "${_PSQL} -f -" <<EOF
   show stats_temp_directory
 EOF
 )
-PG_STATS_TEMP_DIRECTORY="stats_temp_directory: ${PG_STATS_TEMP_DIRECTORY}"
+PG_STATS_TEMP_DIR="stats_temp_directory: ${PG_STATS_TEMP_DIR}"
 
-PG_LOG_DIRECTORY=$(ssh "$HOST" "${_PSQL} -f -" <<EOF
+PG_LOG_DIR=$(ssh "$HOST" "${_PSQL} -f -" <<EOF
   show log_directory
 EOF
 )
-PG_LOG_DIRECTORY="log_directory: ${PG_LOG_DIRECTORY}"
+PG_LOG_DIR="log_directory: ${PG_LOG_DIR}"
 
 PG_TABLESPSACES_DIRS=$(ssh "$HOST" "${_PSQL} -f -" <<EOF
   SELECT pg_catalog.pg_tablespace_location(oid)
@@ -37,15 +36,24 @@ else
   PG_WAL_DIR="${PG_DATA_DIR}/pg_xlog"
 fi
 
-local dir key value
-for dir in "$PG_DATA_DIR"; do
+buf="{ "
+for dir in "$PG_DATA_DIR" "$PG_STATS_TEMP_DIR" "$PG_STATS_TEMP_DIR" "$PG_LOG_DIR" ; do
   while IFS=": " read -r key value; do
-    # save non-empty (to not override by empty)
+    # save non-empty (to not override by empty strings)
     [[ ! -z "$key" ]] && NAME="$key" # postgres variable
-    [[ ! -z "$value" ]] && VALUE="$value"
+    [[ ! -z "$value" ]] && PG_PATH="$value"
   done <<< ${dir}
-  echo "key: $NAME, value: $VALUE"
+ 
+  #echo "key: $NAME, value: $PG_PATH"
+
+  cmd_raw_result=$(ssh "$HOST" "df -T \"${PG_PATH}\"")
+
+  obj="\"${NAME}\": { path: \"${PG_PATH}\", df_raw_result: \"${cmd_raw_result}\" }"
+  buf=" ${buf} ${obj}, "
 
 done
 
-set +x
+buf="${buf} }"
+
+jq -n "$buf"
+
