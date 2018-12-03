6f4ba66717d957e7bc3afb702bff5034b1aa6096
dmius <uddm@mail.ru>
2018-12-03 12:02:55 +0300

    Use tagged url, connection string fixed

--- resources/checks/F001_heap_bloat.sh
+++ resources/checks/F001_heap_bloat.sh
@@ -1,6 +1,6 @@
-sql=$(curl -s -L https://github.com/NikolayS/postgres_dba/raw/master/sql/b1_table_estimation.sql | awk '{gsub("; *$", "", $0); print $0}')
+sql=$(curl -s -L https://raw.githubusercontent.com/NikolayS/postgres_dba/4.0/sql/b1_table_estimation.sql | awk '{gsub("; *$", "", $0); print $0}')
 
-ssh ${HOST} "psql -U postila_ru -t -f - " <<SQL
+ssh ${HOST} "${_PSQL} ${PSQL_CONN_OPTIONS} -f -" <<SQL
 with data as (
 $sql
 )
