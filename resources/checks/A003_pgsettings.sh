c68830972a38578a2e2aefdacd1ccb7f5582c097
Victor Yagofarov <xnasx@yandex.ru>
2018-12-04 15:17:42 +0300

    fix results

--- resources/checks/A003_pgsettings.sh
+++ resources/checks/A003_pgsettings.sh
@@ -1,2 +1,2 @@
 # Collect pg_settings artifact
-ssh ${HOST} "${_PSQL} -c \"select json_object_agg(s.name, s) from pg_settings s\""
+${CHECK_HOST_CMD} "${_PSQL} -c \"select json_object_agg(s.name, s) from pg_settings s\""
--- resources/checks/A004_cluster_info.sh
+++ resources/checks/A004_cluster_info.sh
@@ -1,7 +1,7 @@
 # Collect pg cluster info
 main_sql=$(curl -s -L https://raw.githubusercontent.com/NikolayS/postgres_dba/4.0/sql/0_node.sql | awk '{gsub("; *$", "", $0); print $0}')
 
-pgver=$(ssh ${HOST} "${_PSQL} -c \"SHOW server_version\"")
+pgver=$(${CHECK_HOST_CMD} "${_PSQL} -c \"SHOW server_version\"")
 
 vers=(${pgver//./ })
 majorVer=${vers[0]}
@@ -9,28 +9,21 @@ majorVer=${vers[0]}
 prepare_sql=""
 
 if [[ $majorVer -lt 10 ]]; then
-#  echo "Version less 10"
-  prepare_sql=$(cat <<EOF
+  #  echo "Version less 10"
+  prepare_sql="
 \set postgres_dba_last_wal_receive_lsn pg_last_xlog_receive_location
 \set postgres_dba_last_wal_replay_lsn pg_last_xlog_replay_location
-\set postgres_dba_is_wal_replay_paused pg_is_xlog_replay_paused
-
-EOF
-)
-
+\set postgres_dba_is_wal_replay_paused pg_is_xlog_replay_paused "
 else 
-#  echo "Version more or equal 10"
-  prepare_sql=$(cat <<EOF
+  #  echo "Version greater or equal 10"
+  prepare_sql="
 \set postgres_dba_last_wal_receive_lsn pg_last_wal_receive_lsn
 \set postgres_dba_last_wal_replay_lsn pg_last_wal_replay_lsn
-\set postgres_dba_is_wal_replay_paused pg_is_wal_replay_paused
-
-EOF 
-)
+\set postgres_dba_is_wal_replay_paused pg_is_wal_replay_paused"
 
 fi
 
-ssh ${HOST} "${_PSQL} -f - " <<SQL
+${CHECK_HOST_CMD} "${_PSQL} -f - " <<SQL
 $prepare_sql
 with data as (
 $main_sql
@@ -38,3 +31,4 @@ $main_sql
 select json_object_agg(data.metric, data) as json from data where data.metric not like '------%';
 
 SQL
+
