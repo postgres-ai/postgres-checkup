df4d3e4f9df3bce8a236e80263ccb47b2f972a6c
Victor Yagofarov <xnasx@yandex.ru>
2018-12-07 13:56:40 +0300

    fix changes

--- resources/checks/G002_current_connections.sh
+++ resources/checks/G002_current_connections.sh
@@ -1,14 +1,19 @@
 # Current Activity: count of current connections grouped by database, user name, state
 
+source ~/tmp/wrapper.sh
+
 sql=$(curl -s -L https://raw.githubusercontent.com/NikolayS/postgres_dba/4.0/sql/a1_activity.sql | awk '{gsub("; *$", "", $0); print $0}')
 
-${CHECK_HOST_CMD} "${_PSQL} ${PSQL_CONN_OPTIONS} -f -" <<SQL
+data=$(${CHECK_HOST_CMD} "${_PSQL} ${PSQL_CONN_OPTIONS} -f -" <<SQL
 with data as (
   $sql
 ),
 num_data as (
   select row_number() over () num, data.* from data
 )
-select json_object_agg(num, num_data) as json from num_data
-SQL
+select * from num_data
+SQL)
+
+data="{ \"raw\": \"${data}\" }"
+jq -rn "${data}"
 
