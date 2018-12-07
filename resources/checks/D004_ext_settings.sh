ee45d3fc64a52f8a4f8a193cf27328868caf8f9d
dmius <uddm@mail.ru>
2018-12-07 13:42:28 +0300

    D004 check with template, A005 check extended

---
+++ resources/checks/D004_ext_settings.sh
@@ -0,0 +1,10 @@
+# Collect pg_settings artifact
+${CHECK_HOST_CMD} "${_PSQL} ${PSQL_CONN_OPTIONS} -f -" <<SQL
+with stat_statements as (
+  select json_object_agg(pg_settings.name, pg_settings) as json from pg_settings where name ~ 'pg_stat_statements'
+), kcache as (
+  select json_object_agg(pg_settings.name, pg_settings) as json from pg_settings where name ~ 'pg_stat_kcache'
+)
+select json_build_object('pg_stat_statements', (select * from stat_statements), 'kcache', (select * from kcache));
+SQL
+
