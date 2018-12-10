f27147e2c5f360bd458c37a0a83a0abe1648277d
dmius <uddm@mail.ru>
2018-12-10 09:36:26 +0300

    H001 reworked

--- resources/checks/H001_unused_indexes.sh
+++ resources/checks/H001_unused_indexes.sh
@@ -15,13 +15,8 @@ with indexes as (
   select json_agg(jsondata.json) from (select run_in_separate_transactions as json from deploy) jsondata
 ), revert_code as (
   select json_agg(jsondata.json) from (select run_in_separate_transactions as json from revert) jsondata
-), indexes_detail as (
-  select i.*,
-    (select run_in_separate_transactions from deploy where run_in_separate_transactions like '%' || i.indexname || '%') as drop_code,
-    (select run_in_separate_transactions from revert where run_in_separate_transactions like '%' || i.indexname || '%') as revert_code  
-    from indexes i
 ), unsed_indexes as (
-  select json_object_agg(indexes_detail."indexname", indexes_detail) as json from indexes_detail
+  select json_object_agg(indexes."indexname", indexes) as json from indexes
 )
 select json_build_object('indexes', (select * from unsed_indexes), 'drop_code', (select * from deploy_code), 'revert_code', (select * from revert_code));
 SQL
