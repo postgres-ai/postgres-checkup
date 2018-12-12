ebce9043d9ce3b55255e3061b06e4b51b22c49a7
Victor Yagofarov <xnasx@yandex.ru>
2018-12-12 14:57:05 +0300

    fix changes before branch switch

--- resources/checks/A005_extensions.sh
+++ resources/checks/A005_extensions.sh
@@ -1,12 +1,26 @@
-# Collect pg_settings artifact
+# Collect extensions and their settings
+
 sql=$(curl -s -L https://raw.githubusercontent.com/NikolayS/postgres_dba/4.0/sql/e1_extensions.sql | awk '{gsub("; *$", "", $0); print $0}')
 
-${CHECK_HOST_CMD} "${_PSQL} ${PSQL_CONN_OPTIONS} -f -" <<SQL
-with data as (
-$sql
-), withsettins as (
-    select data.*, (select json_object_agg(pg_settings.name, pg_settings) from pg_settings where name ~ data.name) as settings from data
-)
-select json_object_agg(withsettins.name, withsettins) as json from withsettins;
-SQL
+dbs=$(${CHECK_HOST_CMD} "${_PSQL} -f - " <<SQL
+select datname from pg_database where datname not in ('template0', 'template1', 'postgres')
+SQL)
+
+result="{ \"databases\": null }"
+
+for cur_db in ${dbs}; do
+
+  object=$(${CHECK_HOST_CMD} "${_PSQL} -d "$cur_db" -f -" <<SQL
+    with data as (
+    $sql
+    ), withsettins as (
+        select data.*, (select json_object_agg(name, setting) from pg_settings where name ~ data.name) as settings from data
+    )
+    select json_object_agg(withsettins.name, withsettins) as json from withsettins;
+SQL)
+
+  result=$(jq --arg db "${cur_db}" --argjson obj "$object" -r '.databases += { ($db): $obj }' <<<"${result}")
+done
+
+jq -r . <<<"$result"
 
