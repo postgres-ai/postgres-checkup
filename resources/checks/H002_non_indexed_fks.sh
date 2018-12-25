411d105bd1b95d0d50cca9c08148321ddc1645f2
dmius <uddm@mail.ru>
2018-12-25 16:34:34 +0300

    H002 check and template reworked

--- resources/checks/H002_non_indexed_fks.sh
+++ resources/checks/H002_non_indexed_fks.sh
@@ -2,21 +2,13 @@
 
 sql=$(curl -s -L https://raw.githubusercontent.com/NikolayS/postgres_dba/master/sql/i3_non_indexed_fks.sql | awk '{gsub("; *$", "", $0); print $0}')
 
-data=$(${CHECK_HOST_CMD} "${_PSQL} -f -" <<SQL
--- disble psql CLI options '-A -t' for this report
-\pset tuples_only
-\pset format aligned
-\pset expanded
-
+${CHECK_HOST_CMD} "${_PSQL} -f -" <<SQL
 with data as (
   $sql
 ),
 num_data as (
   select row_number() over () num, data.* from data
 )
-select * from num_data as index
-SQL)
-
-data="{ \"raw\": \"${data}\" }"
-jq -n "${data}"
+select json_object_agg(num_data.num, num_data) from num_data
+SQL
 
