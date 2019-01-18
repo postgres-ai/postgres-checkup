4f55e66fb9f97c920dadcdcdec51b3b47c8fb01f
Victor Yagofarov <xnasx@yandex.ru>
2019-01-18 14:25:45 +0300

    fix table creation for h002

--- .ci/h002.sql
+++ .ci/h002.sql
@@ -1,4 +1,4 @@
-create table t1 as select id::int8 from generate_series(1, 10000000) _(id);
+create table t1 as select id::int8 from generate_series(0, 1000000) _(id);
 alter table t1 add primary key (id);
-create table t2 as select id, (random() * 10000000)::int8 as t1_id from generate_series(1, 10000000) _(id);
+create table t2 as select id, (random() * 1000000)::int8 as t1_id from generate_series(1, 1000000) _(id);
 alter table t2 add constraint fk_t2_t1 foreign key (t1_id) references t1(id);
