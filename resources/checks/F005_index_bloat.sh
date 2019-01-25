4b46d6c5dcec5046c808e8b6b95c8118df8a4f1a
dmius <uddm@mail.ru>
2019-01-25 09:30:20 +0300

    F004: Bloat estimate instead Bloat + sort sign moved

--- resources/checks/F005_index_bloat.sh
+++ resources/checks/F005_index_bloat.sh
@@ -1,4 +1,4 @@
-sql=$(curl -s -L https://raw.githubusercontent.com/NikolayS/postgres_dba/4.0/sql/b2_btree_estimation.sql | awk '{gsub("; *$", "", $0); print $0}')
+sql=$(curl -s -L https://raw.githubusercontent.com/NikolayS/postgres_dba/f1effb54dcfcc7075960a3a51a412d4d2796064a/sql/b2_btree_estimation.sql | awk '{gsub("; *$", "", $0); print $0}')
 
 ${CHECK_HOST_CMD} "${_PSQL} -f -" <<SQL
 with data as (
