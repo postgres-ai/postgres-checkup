245d9fa2fe56262ea9ff62a699b581e15faeb347
Oleg Gurov <oleg@postgres.ai>
2019-03-01 01:04:38 +0300

    L003 Integer (int2, int4) out-of-range risks in PKs

---
+++ pghrep/templates/L003.tpl
@@ -0,0 +1,25 @@
+# {{ .checkId }} Integer (int2, int4) out-of-range risks in PKs #
+
+## Observations ##
+Data collected: {{ DtFormat .timestamptz }}  
+Current database: {{ .database }}  
+{{ if .hosts.master }}
+{{ if and (index .results .hosts.master) (index (index .results .hosts.master) "data") }}
+### Master (`{{.hosts.master}}`) ###
+Table | PK | Type | Current max value | &#9660;&nbsp;Capacity used, %
+------|----|------|-------------------|-------------------------------
+{{ range $i, $key := (index (index (index .results .hosts.master) "data") "_keys") }}
+{{- $value := (index (index (index $.results $.hosts.master) "data") $key) -}}
+{{ index $value "Table"}} | {{ index $value "PK"}} | {{ index $value "Type"}} | {{- RawIntFormat (index $value "Current max value")}} | {{ index $value "Capacity used, %"}}
+{{ end }}
+{{- else -}}{{/*Master data*/}}
+No data
+{{- end }}{{/*Master data*/}}
+{{- else -}}{{/*Master*/}}
+No data
+{{ end }}{{/*Master*/}}
+
+## Conclusions ##
+
+
+## Recommendations ##
---
+++ resources/checks/L003_integer_in_pk.sh
@@ -0,0 +1,70 @@
+f_stdout=$(mktemp)
+f_stderr=$(mktemp)
+
+(${CHECK_HOST_CMD} "${_PSQL} -f - " <<SQL
+do \$$
+declare
+  rec record;
+  out text;
+  i numeric;
+  val int8;
+  ratio numeric;
+begin
+  out := '';
+  i := 0;
+  for rec in
+    select
+      c.oid,
+      (select spcname from pg_tablespace where oid = reltablespace) as tblspace,
+      nspname as schema_name,
+      relname as table_name,
+      t.typname,
+      attname
+    from pg_index i
+    join pg_class c on c.oid = i.indrelid
+    left join pg_namespace n on n.oid = c.relnamespace
+    join pg_attribute a on
+      a.attrelid = i.indrelid
+      and a.attnum = any(i.indkey)
+    join pg_type t on t.oid = atttypid 
+    where
+      i.indisprimary
+      and t.typname in ('int2', 'int4')
+      and nspname <> 'pg_toast'
+  loop
+    execute format('select max(%I) from %I.%I;', rec.attname, rec.schema_name, rec.table_name) into val;
+    if rec.typname = 'int4' then
+      ratio := (val::numeric / 2^31)::numeric;
+    elsif rec.typname = 'int2' then
+      ratio := (val::numeric / 2^15)::numeric;
+    else
+      assert false, 'unreachable point';
+    end if;
+    if ratio > 0.00 then -- report only if > 1% of capacity is reached
+      i:= i+1;
+      out := out || '{"' || rec.table_name || '":' || json_build_object(
+            'Table',
+            coalesce(nullif(quote_ident(rec.schema_name), 'public') || '.', '') || quote_ident(rec.table_name),
+            'PK',
+            rec.attname,
+            'Type',
+            rec.typname,
+            'Current max value',
+            val,
+            'Capacity used, %',
+            round(100 * ratio, 2)
+        ) || '}';
+    end if;
+  end loop;
+  raise info '%', out;
+end;
+\$$ language plpgsql;
+SQL
+) >$f_stdout 2>$f_stderr
+
+result=$(cat $f_stderr)
+result=${result:23:$((${#result}))}
+
+echo "$result" | jq -cs 'sort_by(-(.[]."Capacity used, %"|tonumber)) | .[]' | jq -s add
+
+rm -f "$f_stderr" "$f_stdout"
