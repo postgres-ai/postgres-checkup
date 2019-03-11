f572139e0e750006ffd66e7caf8abcc6d4ed3493
dmius <uddm@mail.ru>
2019-03-11 09:44:41 +0300

    CI test. Improve.

--- .ci/test_db_dump.sh
+++ .ci/test_db_dump.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-for ((i=1; i < 1000; i++))
+# For limit 100 rows per table will generate 110 tables/indexes
+for ((i=1; i < 110; i++))
 do
   psql -h localhost -d dbname -U username -c "create table t_$i as select i from generate_series(1, 1000) _(i);"
   psql -h localhost -d dbname -U username -c "create index concurrently i_$i on t_$i(i);"
