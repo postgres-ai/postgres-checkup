3f0a34b07aced846dfd45680ad24a6393befde48
Oleg Gurov <oleg@postgres.ai>
2019-03-14 19:04:29 +0300

    using of ROWS_LIMIT

--- resources/checks/L003_integer_in_pk.sh
+++ resources/checks/L003_integer_in_pk.sh
@@ -71,6 +71,6 @@ SQL
 result=$(cat $f_stderr)
 result=${result:23:$((${#result}))}
 
-echo "$result" | jq -cs 'sort_by(-(.[]."Capacity used, %"|tonumber)) | limit(100;.[])' | jq -s add
+echo "$result" | jq -cs --arg ROWS_LIMIT "$ROWS_LIMIT" 'sort_by(-(.[]."Capacity used, %"|tonumber)) | limit($ROWS_LIMIT;.[])' | jq -s add
 
 rm -f "$f_stderr" "$f_stdout"
