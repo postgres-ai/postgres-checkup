7fb75c936e511e57d74656d998b0cfd6bac7ccdf
Oleg <oleg@postgres.ai>
2019-04-12 07:15:04 +0000

    Query optimization refs #343

--- .ci/test_db_dump.sql
+++ .ci/test_db_dump.sql
@@ -38,12 +38,6 @@ set statement_timeout to '20ms';
 create index concurrently i_invalid on test_schema.t_with_invalid_index(i);
 set statement_timeout to 0;
 
--- H003 non indexed fks 
-create table t_fk_1 as select id::int8 from generate_series(0, 1000000) _(id);
-alter table t_fk_1 add primary key (id);
-create table t_fk_2 as select id, (random() * 1000000)::int8 as t1_id from generate_series(1, 1000000) _(id);
-alter table t_fk_2 add constraint fk_t2_t1 foreign key (t1_id) references t_fk_1(id);
-
 -- Bloat level
 create table bloated as select i from generate_series(1, 100000) _(i); 
 create index i_bloated on bloated(i); 
