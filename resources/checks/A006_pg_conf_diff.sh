e294100a8cddae732fbcfc5789a0ae862913cbf9
Dmitry <uddm@mail.ru>
2019-04-26 13:16:55 +0000

    F005 order, A006 exclude, K00X s/sec fixed

--- resources/checks/A006_pg_conf_diff.sh
+++ resources/checks/A006_pg_conf_diff.sh
@@ -1,6 +1,19 @@
 # Collect pg_settings and pg_config values data to compare with same values from other domains
 # Require comparation in plugin
-pg_settings=$(${CHECK_HOST_CMD} "${_PSQL} -c \"select json_object_agg(s.name, s) from (select * from pg_settings s order by name) s;\"")
-pg_config=$(${CHECK_HOST_CMD} "${_PSQL} -c \"select json_object_agg(s.name, s) from (select * from pg_config s order by name) s;\"")
-echo '{ "pg_settings": '$pg_settings', "pg_config":'$pg_config'}'
-
+${CHECK_HOST_CMD} "${_PSQL} -f -" <<SQL
+with settings as (
+  select
+    json_object_agg(s.name, s)
+  from (
+    select *
+    from pg_settings s
+    where
+      name not in ('linux_hz', 'pg_stat_kcache.linux_hz', 'transaction_read_only')
+    order by name) s
+), configs as (
+  select
+    json_object_agg(s.name, s)
+  from (select * from pg_config s order by name) s
+)
+select json_build_object('pg_settings', (select * from settings), 'pg_config', (select * from configs));
+SQL
