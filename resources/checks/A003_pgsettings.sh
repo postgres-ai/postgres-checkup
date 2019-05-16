40ff3122b2429b0b8c93ea0d59431cfddcd8c3ba
Nikolay Samokhvalov <gitlab@samokhvalov.com>
2019-05-16 19:43:42 +0000

    Run A003 without setting statement_timeout.

--- resources/checks/A003_pgsettings.sh
+++ resources/checks/A003_pgsettings.sh
@@ -1,2 +1,2 @@
 # Collect pg_settings artifact
-${CHECK_HOST_CMD} "${_PSQL} -c \"select json_object_agg(s.name, s) from (select * from pg_settings s order by category, name) s;\""
+${CHECK_HOST_CMD} "${_PSQL_NO_TIMEOUT} -c \"select json_object_agg(s.name, s) from (select * from pg_settings s order by category, name) s;\""
