1d7145754ee2025360fdb6580fb33e60eefca8ad
Dmitry Udalov <uddm@mail.ru>
2019-06-11 11:59:04 +0300

    F001: P1 conclusion and recommendation

--- pghrep/src/checkup/f001/f001.go
+++ pghrep/src/checkup/f001/f001.go
@@ -2,24 +2,60 @@ package f001
 
 import (
 	"encoding/json"
+	"strconv"
+	"strings"
 
 	checkup ".."
-	"../../log"
 )
 
 // Case codes
 const F001_AUTOVACUUM_NOT_TUNED string = "F001_AUTOVACUUM_NOT_TUNED"
+const F001_AUTOVACUUM_TUNE_RECOMMENDATION string = "F001_AUTOVACUUM_TUNE_RECOMMENDATION"
 
 func F001Process(report F001Report) checkup.ReportResult {
 	var result checkup.ReportResult
-	for host, hostData := range report.Results {
-		for setting, settingData := range hostData.Data.Settings.GlobalSettings {
-			log.Dbg(host, setting, settingData)
+
+	for _, hostData := range report.Results {
+		autovacuumVacuumScaleFactor := hostData.Data.Settings.GlobalSettings["autovacuum_vacuum_scale_factor"]
+		autovacuumVacuumThreshold := hostData.Data.Settings.GlobalSettings["autovacuum_vacuum_threshold"]
+		autovacuumAnalyzeScaleFactor := hostData.Data.Settings.GlobalSettings["autovacuum_analyze_scale_factor"]
+		autovacuumAnalyzeThreshold := hostData.Data.Settings.GlobalSettings["autovacuum_analyze_threshold"]
+		autovacuumVacuumCostDelay := hostData.Data.Settings.GlobalSettings["autovacuum_vacuum_cost_delay"]
+		autovacuumVacuumCostLimit := hostData.Data.Settings.GlobalSettings["autovacuum_vacuum_cost_limit"]
+
+		autovacuumVacuumScaleFactorValue, _ := strconv.ParseFloat(autovacuumVacuumScaleFactor.Setting, 64)
+		autovacuumVacuumThresholdValue, _ := strconv.ParseFloat(autovacuumVacuumThreshold.Setting, 64)
+		autovacuumAnalyzeScaleFactorValue, _ := strconv.ParseFloat(autovacuumAnalyzeScaleFactor.Setting, 64)
+		autovacuumAnalyzeThresholdValue, _ := strconv.ParseFloat(autovacuumAnalyzeThreshold.Setting, 64)
+		autovacuumVacuumCostDelayValue, _ := strconv.ParseFloat(autovacuumVacuumCostDelay.Setting, 64)
+		autovacuumVacuumCostLimitValue, _ := strconv.ParseFloat(autovacuumVacuumCostLimit.Setting, 64)
+
+		var defaultValues []string
+
+		if autovacuumVacuumScaleFactorValue == 0.2 && autovacuumVacuumThresholdValue == 50 {
+			defaultValues = append(defaultValues, "    - `autovacuum_vacuum_scale_factor` = 0.2 and `autovacuum_vacuum_threshold` = 50  ")
+
 		}
-		for setting, settingData := range hostData.Data.Settings.TableSettings {
-			log.Dbg(host, setting, settingData)
+
+		if autovacuumAnalyzeScaleFactorValue == 0.1 && autovacuumAnalyzeThresholdValue == 50 {
+			defaultValues = append(defaultValues, "    - `autovacuum_analyze_scale_factor` = 0.1 and `autovacuum_analyze_threshold` = 50  ")
+		}
+
+		if autovacuumVacuumCostDelayValue == 20 && autovacuumVacuumCostLimitValue == -1 {
+			defaultValues = append(defaultValues, "    - `autovacuum_vacuum_cost_delay` = 20 and `autovacuum_vacuum_cost_limit` = -1  ")
+		}
+
+		if len(defaultValues) > 0 {
+			result.P1 = true
+			result.AppendConclusion(F001_AUTOVACUUM_NOT_TUNED, MSG_AUTOVACUUM_NOT_TUNED_CONCLUSION, strings.Join(checkup.LimitList(defaultValues), ""))
+			result.AppendRecommendation(F001_AUTOVACUUM_NOT_TUNED, MSG_AUTOVACUUM_NOT_TUNED_RECOMMENDATION)
 		}
 	}
+
+	if result.P1 || result.P2 {
+		result.AppendRecommendation(F001_AUTOVACUUM_TUNE_RECOMMENDATION, MSG_AUTOVACUUM_TUNE_RECOMMENDATION)
+	}
+
 	return result
 }
 
@@ -34,5 +70,5 @@ func F001PreprocessReportData(data map[string]interface{}) {
 	if len(result.Recommendations) > 0 {
 	}
 	// update data and file
-	//checkup.SaveReportResult(data, result)
+	checkup.SaveReportResult(data, result)
 }
--- pghrep/src/main.go
+++ pghrep/src/main.go
@@ -25,6 +25,7 @@ import (
 	"./checkup/a002"
 	"./checkup/a006"
 	"./checkup/a008"
+	"./checkup/f001"
 	"./checkup/f002"
 	"./checkup/f004"
 	"./checkup/f005"
@@ -426,6 +427,8 @@ func preprocessReportData(checkId string, data map[string]interface{}) {
 		f004.F004PreprocessReportData(data)
 	case "F005":
 		f005.F005PreprocessReportData(data)
+	case "F001":
+		f001.F001PreprocessReportData(data)
 	}
 	return
 }
