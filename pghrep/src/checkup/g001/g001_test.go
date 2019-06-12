2a4ae3c9b3a7a5cc063e5b67dfa793262c2c3497
Dmitry <uddm@mail.ru>
2019-06-12 08:06:15 +0000

    G001: generating P1 conclusion and recommendations about the value of shared_buffers implemented

---
+++ pghrep/src/checkup/g001/g001_test.go
@@ -0,0 +1,121 @@
+package g001
+
+import (
+	"fmt"
+	"testing"
+
+	checkup ".."
+	"../a001"
+)
+
+func TestG001Success(t *testing.T) {
+	fmt.Println(t.Name())
+	// G001
+	var report G001Report
+	var hostResult G001ReportHostResult
+
+	hostResult.Data = map[string]G001Setting{
+		"shared_buffers": G001Setting{
+			Name:    "shared_buffers",
+			Setting: "4194304",
+			Unit:    "8kB",
+		},
+	}
+	report.Results = G001ReportHostsResults{"test-host": hostResult}
+
+	// A001
+	var a001Report a001.A001Report
+	var a001HostResult a001.A001ReportHostResult
+	a001HostResult.Data = a001.A001ReportHostResultData{
+		Ram: a001.A001ReportRam{
+			MemTotal: "65888240 kB",
+		},
+	}
+	a001Report.Results = a001.A001ReportHostsResults{"test-host": a001HostResult}
+
+	result := G001Process(report, a001Report)
+
+	if result.P1 || result.P2 || result.P3 &&
+		checkup.ResultInList(result.Conclusions, G001_SHARED_BUFFERS_NOT_OPTIMAL) {
+		t.Fatal()
+	}
+
+	checkup.PrintResultConclusions(result)
+	checkup.PrintResultRecommendations(result)
+}
+
+func TestG001Low(t *testing.T) {
+	fmt.Println(t.Name())
+	// G001
+	var report G001Report
+	var hostResult G001ReportHostResult
+
+	hostResult.Data = map[string]G001Setting{
+		"shared_buffers": G001Setting{
+			Name:    "shared_buffers",
+			Setting: "1235404",
+			Unit:    "8kB",
+		},
+	}
+	report.Results = G001ReportHostsResults{"test-host": hostResult}
+
+	// A001
+	var a001Report a001.A001Report
+	var a001HostResult a001.A001ReportHostResult
+	a001HostResult.Data = a001.A001ReportHostResultData{
+		Ram: a001.A001ReportRam{
+			MemTotal: "65888240 kB",
+		},
+	}
+	a001Report.Results = a001.A001ReportHostsResults{"test-host": a001HostResult}
+
+	result := G001Process(report, a001Report)
+
+	if !result.P1 ||
+		!checkup.ResultInList(result.Conclusions, G001_SHARED_BUFFERS_NOT_OPTIMAL) ||
+		!checkup.ResultInList(result.Recommendations, G001_SHARED_BUFFERS_NOT_OPTIMAL) ||
+		!checkup.ResultInList(result.Recommendations, G001_TUNE_SHARED_BUFFERS) {
+		t.Fatal()
+	}
+
+	checkup.PrintResultConclusions(result)
+	checkup.PrintResultRecommendations(result)
+}
+
+func TestG001High(t *testing.T) {
+	fmt.Println(t.Name())
+	// G001
+	var report G001Report
+	var hostResult G001ReportHostResult
+
+	hostResult.Data = map[string]G001Setting{
+		"shared_buffers": G001Setting{
+			Name:    "shared_buffers",
+			Setting: "6753544",
+			Unit:    "8kB",
+		},
+	}
+	report.Results = G001ReportHostsResults{"test-host": hostResult}
+
+	// A001
+	var a001Report a001.A001Report
+	var a001HostResult a001.A001ReportHostResult
+	a001HostResult.Data = a001.A001ReportHostResultData{
+		Ram: a001.A001ReportRam{
+			MemTotal: "65888240 kB",
+		},
+	}
+	a001Report.Results = a001.A001ReportHostsResults{"test-host": a001HostResult}
+
+	result := G001Process(report, a001Report)
+
+	if !result.P1 ||
+		!checkup.ResultInList(result.Conclusions, G001_SHARED_BUFFERS_NOT_OPTIMAL) ||
+		!checkup.ResultInList(result.Recommendations, G001_SHARED_BUFFERS_NOT_OPTIMAL) ||
+		!checkup.ResultInList(result.Recommendations, G001_TUNE_SHARED_BUFFERS) {
+		t.Fatal()
+	}
+
+	checkup.PrintResultConclusions(result)
+	checkup.PrintResultRecommendations(result)
+}
