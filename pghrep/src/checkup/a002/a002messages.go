dca9703ebd4544df034466d8534a8c10e2a546cc
Anatoly Stansler <anatoly@postgres.ai>
2019-06-24 19:18:38 +0300

    fix: add link to last year supported

--- pghrep/src/checkup/a002/a002messages.go
+++ pghrep/src/checkup/a002/a002messages.go
@@ -10,7 +10,8 @@ const MSG_NOT_SUPPORTED_VERSION_RECOMMENDATION string = "[P1] Please upgrade Pos
 	"versions supported by the community and PGDG. To minimize downtime, consider using pg_upgrade or one " +
 	"of the solutions for logical replication.  \n"
 const MSG_LAST_YEAR_SUPPORTED_VERSION_CONCLUSION string = "[P2] Postgres community and PGDG will stop supporting version `%s`" +
-	" within the next 12 months (end of life is scheduled `%s`). After that, you will be on your own!  \n"
+	" within the next 12 months (end of life is scheduled `%s`). After that, you will be on your own!" +
+	" Read more: [Versioning Policy](https://www.postgresql.org/support/versioning/).  \n"
 const MSG_SUPPORTED_VERSION_CONCLUSION string = "Postgres major version being used is `%s` and it is " +
 	"currently supported by Postgres community and PGDG (end of life is scheduled %s). It means that in case " +
 	"of bugs and security issues, updates (new minor versions) with fixes will be released and available for use." +
