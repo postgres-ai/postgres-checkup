c37a978598f3e96c42ac7a35b62183a00620fe37
Dmitry <uddm@mail.ru>
2019-07-05 18:18:45 +0000

    Apply suggestion to .ci/test_db_dump.sh

--- .ci/test_db_dump.sh
+++ .ci/test_db_dump.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# Generate more than 100 tables with index to check limitation of table rows count
+# Generate more than 100 tables with index to check that
+# the limitation of .md reports' rows counts work as expected.
 for ((i=1; i < 110; i++))
 do
   psql -d dbname -U test_user -c "create table t_$i as select i from generate_series(1, 1000) _(i);"
