5293f26d26e311a9990007fdba08c65142f03670
Nikolay Samokhvalov <gitlab@samokhvalov.com>
2019-07-08 03:43:25 +0000

    Improve wording in F002

--- pghrep/src/checkup/f002/f002messages.go
+++ pghrep/src/checkup/f002/f002messages.go
@@ -2,7 +2,7 @@ package f002
 
 const MSG_RISKS_ARE_HIGH_CONCLUSION_1 string = "[P1] Risks of transaction ID wraparound are high for:  \n" +
 	"%s  \n" +
-	"Approaching 100%% leads to downtime: the system will shut down and refuse to start any new transactions.  \n"
+	"Approaching 100%% will lead to long downtime: the system will shut down and refuse to start until VACUUM is performed in single user mode.  \n"
 const MSG_RISKS_ARE_HIGH_CONCLUSION_2 string = "More on this topic:  \n" +
 	"    - [PostgreSQL Documentation. 24.1.5. Preventing Transaction ID Wraparound Failures](https://www.postgresql.org/docs/current/routine-vacuuming.html#VACUUM-FOR-WRAPAROUND)  \n" +
 	"    - [The Internals of PostgreSQL. Chapter 5. Concurrency Control. 5.10.1. FREEZE Processing](http://www.interdb.jp/pg/pgsql05.html#_5.10.1.)  \n" +
@@ -11,6 +11,6 @@ const MSG_RISKS_ARE_HIGH_CONCLUSION_2 string = "More on this topic:  \n" +
 	"    - [What We Learned from the Recent Mandrill Outage](https://mailchimp.com/what-we-learned-from-the-recent-mandrill-outage/) (2019, Mailchimp blog)  \n" +
 	"    - [Managing Transaction ID Exhaustion (Wraparound) in PostgreSQL](https://info.crunchydata.com/blog/managing-transaction-id-wraparound-in-postgresql) (2019, Crunchy Data blog)  \n"
 
-const MSG_RISKS_ARE_HIGH_RECOMMENDATION string = "[P1] To minimize risks of transaction ID wraparound do the following:  \n" +
-	"1. Run `VACUUM FREEZE` for mentioned tables.  \n" +
-	"1. Perform autovacuum tuning to ensure that autovacuum has enough resources and runs often enough to minimize risks of transaction ID wraparound. Read articles provided in the \"Conclusions\" section for more details. "
+const MSG_RISKS_ARE_HIGH_RECOMMENDATION string = "[P1] To minimize risks of transaction ID wraparound consider the following:  \n" +
+	"1. Run `VACUUM FREEZE` for all tables considered as risky (see Observations and Conclusions).  \n" +
+	"1. Perform autovacuum tuning to ensure that autovacuum has enough resources and runs often enough to minimize risks of transaction ID wraparound in the future. For more details, read articles provided in  Conclusions. "
