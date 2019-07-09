965ed9b794b4c4f1a97c61d5547481ee8a908920
dmius <uddm@mail.ru>
2019-07-10 00:12:26 +0300

    Apply suggestion

--- resources/checks/A001_system_info.sh
+++ resources/checks/A001_system_info.sh
@@ -43,10 +43,7 @@ function get_mem_info() {
 }
 
 function get_system_info() {
-  local uname_a_cmd='$(uname -a)'
-  local uname_s_cmd='$(uname -s)'
-  local uname_r_cmd='$(uname -r)'
-  local uname_json_data=$(${CHECK_HOST_CMD} "echo '\"raw\":\"'$uname_a_cmd'\", \"operating_system\":\"'$uname_s_cmd'\", \"kernel_release\":\"'$uname_r_cmd'\"'")
+  local uname_json_data=$(${CHECK_HOST_CMD} 'echo \"raw\":\"$(uname -a)\", \"operating_system\":\"$(uname -s)\", \"kernel_release\":\"$(uname -r)\"')
   res_obj="{\"cmd2check\": \"uname -a\", $uname_json_data}"
   OS_INFO=$res_obj #$(jq -n "$res_obj")
 }
