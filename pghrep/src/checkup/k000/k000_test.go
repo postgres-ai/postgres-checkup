c9f4d2a7e3e5157170fac2a1262621b01396f911
dmius <uddm@mail.ru>
2019-07-12 21:59:18 +0300

    fix: show every host only once in conlusion of K003

--- pghrep/src/checkup/k000/k000_test.go
+++ pghrep/src/checkup/k000/k000_test.go
@@ -2,6 +2,7 @@ package k000
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 
 	checkup ".."
@@ -59,7 +60,7 @@ func TestK000TotalExcess(t *testing.T) {
 	checkup.PrintResultRecommendations(result)
 }
 
-func TestK000TotalExcessNodes(t *testing.T) {
+func TestK000HostTwice(t *testing.T) {
 	fmt.Println(t.Name())
 
 	var report K000Report
@@ -68,6 +69,9 @@ func TestK000TotalExcessNodes(t *testing.T) {
 		Queries: map[string]K00Query{
 			"1": K00Query{
 				RatioTotalTime: 40.0,
+			},
+			"2": K00Query{
+				RatioTotalTime: 34.0,
 			},
 		},
 	}
@@ -80,6 +84,14 @@ func TestK000TotalExcessNodes(t *testing.T) {
 		!checkup.ResultInList(result.Conclusions, K000_EXCESS_QUERY_TOTAL_TIME) ||
 		!checkup.ResultInList(result.Recommendations, K000_EXCESS_QUERY_TOTAL_TIME) {
 		t.Fatal()
+	}
+
+	conclusion, err2 := checkup.GetResultItem(result.Conclusions, K000_EXCESS_QUERY_TOTAL_TIME)
+	if err2 != nil || strings.Contains(conclusion.Message, "`test-host-1`, `test-host-1`") ||
+		strings.Contains(conclusion.Message, "`test-host-2`, `test-host-2`") ||
+		strings.Contains(conclusion.Message, "`test-host-2` and `test-host-2`") ||
+		strings.Contains(conclusion.Message, "`test-host-1` and `test-host-1`") {
+		t.Fatal("Same host twice in conlusion")
 	}
 
 	checkup.PrintResultConclusions(result)
