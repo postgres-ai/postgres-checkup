e4736db62e04879bd66004c76b58c27812079c9a
Dmitry <uddm@mail.ru>
2019-10-29 06:07:46 +0000

    Apply suggestion to pghrep/src/checkup/h002/h002messages.go

--- pghrep/src/checkup/h002/h002messages.go
+++ pghrep/src/checkup/h002/h002messages.go
@@ -12,7 +12,7 @@ const MSG_UNUSED_INDEXES_FOUND_R1 string = "Use the database migration provided 
 const MSG_UNUSED_INDEXES_FOUND_R2 string = "Be careful dropping the indexes. If you have multiple setups of your software, the analysis " +
 	"of just a single setup might be not enough. Some indexes might be used (and therefore, needed) only on a limited number of setups. " +
 	"Also, in some cases, developers prepare indexes for new features in advance – in such cases, dropping those indexes is not a good idea."
-const MSG_UNUSED_INDEXES_FOUND_R3 string = "If there are some doubts, consider a more careful approach. Before actual dropping, indexes " +
+const MSG_UNUSED_INDEXES_FOUND_R3 string = "If there are some doubts, consider a more careful approach. Before actual dropping, " +
 	"are disabled using queries like `UPDATE pg_index SET  indisvalid = false WHERE indexrelid::regclass = (select oid " +
 	"FROM pg_class WHERE relname = 'u_users_email');`. After disabling, the indexes will continue to receive updates but disabled indexes will be not used by queries. In the case of any performance degradations, " +
 	"re-enable the corresponding indexes, setting `indisvalid` to `true`. If everything looks fine, after a significant period of observations, " +
