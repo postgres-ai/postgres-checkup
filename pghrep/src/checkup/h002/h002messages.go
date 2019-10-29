5780dec8363ff00f9455183ffc63eae69a3b3f26
Dmitry <uddm@mail.ru>
2019-10-29 06:06:45 +0000

    Apply suggestion to pghrep/src/checkup/h002/h002messages.go

--- pghrep/src/checkup/h002/h002messages.go
+++ pghrep/src/checkup/h002/h002messages.go
@@ -1,7 +1,7 @@
 package h002
 
 const MSG_UNUSED_INDEXES_FOUND_P2_CONCLUSION string = "[P2] %d unused index(es) have been found and their total size exceeds %.2f%% of the database size."
-const MSG_UNUSED_INDEXES_FOUND_P3_CONCLUSION string = "[P3] There is %d unused index found."
+const MSG_UNUSED_INDEXES_FOUND_P3_CONCLUSION string = "[P3] %d unused index(es) have been found."
 
 const MSG_UNUSED_INDEXES_FOUND_R1 string = "Use the database migration provided below to drop the unused indexes. Keep in mind, that under " +
 	"load, it is recommended to use `DROP INDEX CONCURRENTLY` (and `CREATE INDEX CONCURRENTLY` if reverting is needed) " +
